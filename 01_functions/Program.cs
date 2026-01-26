313c84f105abdee376736250b4fbd6e6b52156c9
Ivan <yeromenkoivan05nl@gmail.com>
2026-01-26 15:06:53 +0100

    02_no_static

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program app = new Program(); // nieuwe variabele: app, type: Program (object van de class Program)
+        app.Run(); // roept de functie Run() aan
+    }
+
+    void Run()
+    {
+        Console.WriteLine("Hello World!");// toont de tekst "Hello World!" in de terminal
     }
 }
