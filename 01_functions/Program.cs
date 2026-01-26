7280812992867d57c2c4d384ee6cabbaac2e6d92
Ivan <yeromenkoivan05nl@gmail.com>
2026-01-26 19:01:09 +0100

    04_function_met_return

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -2,6 +2,15 @@
 
 class Program
 {
+ 
+    string[] vragen = new string[]
+    {
+        "What 1997 N64 video game features James Bond and is named after the 1995 film?",
+        "What arcade game was called Puckman in Japan?",
+        "What is your favorite game?",
+        "Where are you from?"
+    };
+
     static void Main(string[] args)
     {
         Program app = new Program();
@@ -14,10 +23,26 @@ class Program
         Vraag2();
         Vraag3();
         Vraag4();
-        Vraag5();   
+        Vraag5();
+        Vraag6();
+        // Vraag7
+            string antwoord = Vraag7();
+            Console.WriteLine(antwoord);
 
+        string vraag = GetRandomVraag();
+        Console.WriteLine(vraag);
+
+        string antwoordRandom = Console.ReadLine();
+        Console.WriteLine(antwoordRandom);
     }
 
+    string GetRandomVraag()
+    {
+        Random rnd = new Random();
+        int random = rnd.Next(vragen.Length);
+        return GetVraag(random);
+    }
+    
     internal void Vraag1()
     {
         Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
@@ -25,32 +50,52 @@ class Program
         Console.WriteLine(antwoord);
     }
 
-        internal void Vraag2()
+    internal void Vraag2()
     {
         Console.WriteLine("What is your name?");
         string antwoord = Console.ReadLine();
         Console.WriteLine(antwoord);
     }
 
-        internal void Vraag3()
+    internal void Vraag3()
     {
         Console.WriteLine("How old are you?");
         string antwoord = Console.ReadLine();
         Console.WriteLine(antwoord);
     }
 
-        internal void Vraag4()
+    internal void Vraag4()
     {
         Console.WriteLine("Where are you from?");
         string antwoord = Console.ReadLine();
         Console.WriteLine(antwoord);
     }
 
-        internal void Vraag5()
+    internal void Vraag5()
     {
         Console.WriteLine("Do you see me?");
         string antwoord = Console.ReadLine();
         Console.WriteLine(antwoord);
     }
+
+    internal void Vraag6()
+    {
+        Console.WriteLine("Why do round pizzas come in square boxes?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+
+    string Vraag7()
+    {
+        Console.WriteLine("Why do round pizzas come in square boxes?(Return test)");
+        string antwoord = Console.ReadLine();
+        return antwoord;
+    }
+
+    string GetVraag(int vraagIndex)
+    {
+        return vragen[vraagIndex];
+    }
+
 
 }
