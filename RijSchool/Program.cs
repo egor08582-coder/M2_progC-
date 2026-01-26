3b33e10eeda28ce6b486f0aabe6dee63271af56b
Ivan <yeromenkoivan05nl@gmail.com>
2026-01-26 19:43:19 +0100

    00_class_maken

---
+++ RijSchool/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Auto
+{
+    
+}
+
+internal class RijLeraar
+{
+    
+}
+
+internal class Student
+{
+    
+}
+
+internal class LesPakket
+{
+    
+}
+
+internal class RijTest
+{
+    
+}
+
+internal class TheorieTest
+{
+    
+}
+
+internal class Dag
+{
+    
+}
+
+internal class LesUur
+{
+    
+}
