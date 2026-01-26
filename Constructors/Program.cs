99c006ccda80c58b24aba53b20f27cf87407cefc
Ivan <yeromenkoivan05nl@gmail.com>
2026-01-26 20:54:08 +0100

    00_class_maken

---
+++ Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿static void Main(string[] args)
+{
+    Program app = new Program();
+    app.Run();
+}
+
+private void Run()
+{
+    QuizVraag quizVraag = new QuizVraag("hier komt de vraag", "hier komt het antwoord");
+}
+
+class QuizVraagAntwoord
+{
+    internal QuizVraag vraag;
+    internal; bool goed;
+}
+
+class Quiz
+{
+    internal QuizVraag[] vragen;
+    internal QuizVraagAntwoord[] ingevuldeAntwoorden;
+}
+
+class QuizVraag
+{
+    internal string vraag;
+    internal string antwoord;
+
+    internal QuizVraag(string vraag, string antwoord)
+    {
+        this.vraag = vraag;
+        this.antwoord = antwoord;
+    }
+}
