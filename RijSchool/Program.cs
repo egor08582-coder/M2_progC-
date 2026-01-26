a35df84d5c93142473036459dd521249f988790a
Ivan <yeromenkoivan05nl@gmail.com>
2026-01-26 21:43:49 +0100

    02_classes_gebruiken_fix

--- RijSchool/Program.cs
+++ RijSchool/Program.cs
@@ -1,77 +1,79 @@
-﻿static void Main(string[] args)
+﻿class Program
 {
-    Program app = new Program ();
-    app.Run();
+    static void Main(string[] args)
+    {
+        Program app = new Program();
+        app.Run();
+    }
+
+    void Run()
+    {
+        Auto auto1 = new Auto()
+        {
+            automaat = true,
+            kenteken = "AA824H",
+            kilometerStand = 20567,
+            merk = "volkswagen"
+        };
+
+        Auto auto2 = new Auto()
+        {
+            automaat = false,
+            kenteken = "KU786G",
+            kilometerStand = 65739,
+            merk = "volvo"
+        };
+
+        RijLeraar rijLeraar = new RijLeraar()
+        {
+            leeftijd = 30,
+            naam = "John",
+            zzp = false
+        };
+
+        LesUur lesUur = new LesUur()
+        {
+            auto = auto1,
+            rijLeraar = rijLeraar,
+
+            tijd = 1130
+        };
+
+        LesPakket pakket1 = new LesPakket()
+        {
+            urenGekocht = 30,
+            urenVerbruikt = 10,
+            examenPogingen = 0,
+            automaat = true
+        };
+
+        TheorieTest theorie1 = new TheorieTest()
+        {
+            aantalFouten = 2,
+            gehaald = false,
+            afnameDatum = DateTime.Now
+        };
+
+        RijTest rijTest1 = new RijTest()
+        {
+            gehaald = false,
+            afnameDatum = DateTime.Now
+        };
+
+
+        Student student1 = new Student()
+        {
+            naam = "Wanda",
+            leeftijd = 19,
+            lesPakket = pakket1,
+            theorieTest = theorie1,
+            rijTest = rijTest1
+        };
+
+        lesUur.student = student1;
+
+    }
 }
-
-void Run()
-{
-    Auto auto1 = new Auto()
-    {
-        automaat = true,
-        kenteken = "AA824H",
-        kilometerStand = 20567,
-        merk = "volkswagen"
-    };
-
-    Auto auto2 = new Auto()
-    {
-        automaat = false,
-        kenteken = "KU786G",
-        kilometerStand = 65739,
-        merk = "volvo"
-    };
-
-    RijLeraar rijLeraar = new RijLeraar()
-    {
-        leeftijd = 30,
-        naam = "John",
-        zzp = false
-    };
-
-    LesUur lesUur = new LesUur()
-    {
-        auto = auto1,
-        rijLeraar = rijLeraar,
-
-        tijd = 1130
-    };
-
-    LesPakket pakket1 = new LesPakket()
-    {
-        urenGekocht = 30,
-        urenVerbruikt = 10,
-        examenPogingen = 0,
-        automaat = true
-    };
-
-    TheorieTest theorie1 = new TheorieTest()
-    {
-        aantalFouten = 2,
-        gehaald = false,
-        afnameDatum = DateTime.Now
-    };
-
-    RijTest rijTest1 = new RijTest()
-    {
-        gehaald = false,
-        afnameDatum = DateTime.Now
-    };
-
-
-    Student student1 = new Student()
-    {
-        naam = "Wanda",
-        leeftijd = 19,
-        lesPakket = pakket1,
-        theorieTest = theorie1,
-        rijTest = rijTest1
-    };
-    
-    lesUur.student = student1;
-
-}
-
 internal class Auto
 {
     internal bool automaat;
