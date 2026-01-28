309893075a2711d8e095617c2aa7194a64e45c55
Ivan <yeromenkoivan05nl@gmail.com>
2026-01-28 18:41:01 +0100

    01_file_lezen

--- 06_fileio/00_file_toevoegen/Program.cs
+++ 06_fileio/00_file_toevoegen/Program.cs
@@ -8,6 +8,13 @@
 
     void Run()
     {
+        Console.WriteLine(Environment.CurrentDirectory);
 
+        string[] lines = File.ReadAllLines("quiz.txt");
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            Console.WriteLine(lines[i]);
+        }
     }
 }
