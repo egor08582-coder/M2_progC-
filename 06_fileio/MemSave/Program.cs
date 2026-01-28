26f7ac4501f77e1d72047a8c0299eabc1ac0f58e
Ivan <yeromenkoivan05nl@gmail.com>
2026-01-28 18:51:48 +0100

    02_geheugen_bewaren

---
+++ 06_fileio/MemSave/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    string welkomstTekst = "Hello World!";
+    string saveFile = "welkomState.txt";
+
+    static void Main(string[] args)
+    {
+        Program app = new Program();
+        app.Run();
+    }
+
+    void Run()
+    {
+        if (File.Exists(saveFile))
+        {
+            welkomstTekst = File.ReadAllText(saveFile);
+        }
+
+        while (true)
+        {
+            Console.WriteLine(welkomstTekst);
+            Console.WriteLine("Enter a text, then press enter");
+
+            welkomstTekst = Console.ReadLine();
+
+            if (welkomstTekst.Length > 0)
+            {
+                File.WriteAllText(saveFile, welkomstTekst);
+            }
+        }
+    }
+}
